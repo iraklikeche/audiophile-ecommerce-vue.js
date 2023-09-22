2b45b37e86f7c81d63721b3fbfa6a0fb7065563a
iraklikeche <irakli.kechee@gmail.com>
2023-09-22 14:08:15 +0400

    Home page styling

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
 import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
 </script>
 
 <template>
   <div class="flex flex-col font-Manrope">
     <Navbar />
     <RouterView />
+    <Footer />
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,11 +8,11 @@ import data from "../data.json";
       class="grid grid-cols-2 items-center pt-6 gap-20 max-w-7xl mx-auto my-0"
     >
       <div class="py-24">
-        <h4
+        <h2
           class="text-sm text-transform: uppercase tracking-[10px] font-medium text-white opacity-30"
         >
           New Product
-        </h4>
+        </h2>
         <h2
           class="text-transform: uppercase text-6xl text-white mt-8 tracking-[4px]"
         >
@@ -91,11 +91,11 @@ import data from "../data.json";
           <img src="../assets/bg-1.png" class="pt-16" />
         </div>
         <div class="flex flex-col justify-center gap-6 pr-32">
-          <h4
+          <h2
             class="text-6xl text-white font-bold transform: uppercase tracking-[2px]"
           >
             {{ data[5].name }}
-          </h4>
+          </h2>
           <p class="text-base font-medium text-white opacity-50 leading-7">
             Upgrade to premium speakers that are phenomenally built to deliver
             truly remarkable sound.
@@ -110,7 +110,64 @@ import data from "../data.json";
           </div>
         </div>
       </div>
-      <div class="rounded-lg"></div>
+      <div
+        class="container bg-1 flex items-center mt-12 h-[350px] bg-cover bg-center bg-no-repeat rounded-lg"
+      >
+        <div class="pl-24 flex flex-col gap-8">
+          <h2 class="text-3xl font-bold transform: uppercase tracking-[2px]">
+            {{ data[4].name }}
+          </h2>
+          <button
+            class="bg-[#000] hover:bg-[#4C4C4C] transition-colors text-white text-[14px] font-bold tracking-[1px] transform: uppercase px-8 py-4"
+          >
+            See Product
+          </button>
+        </div>
+      </div>
+
+      <div class="grid grid-cols-2 gap-12 mt-12 rounded-lg">
+        <div>
+          <img
+            src="../assets/product-yx1-earphones/desktop/image-gallery-2.jpg"
+            class="rounded-lg w-full"
+          />
+        </div>
+        <div
+          class="flex flex-col justify-center gap-8 bg-[#F1F1F1] pl-24 rounded-lg"
+        >
+          <h2 class="text-3xl font-bold transform: uppercase tracking-[2px]">
+            {{ data[0].name.replace(" Wireless", "") }}
+          </h2>
+          <div>
+            <button
+              class="bg-[#000] hover:bg-[#4C4C4C] transition-colors text-white text-[14px] font-bold tracking-[1px] transform: uppercase px-8 py-4"
+            >
+              See Product
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="pb-32 px-32">
+      <div class="grid grid-cols-2 gap-40 items-center">
+        <div>
+          <h2 class="text-black transform: uppercase text-5xl font-bold">
+            Bringing you the <span class="text-[#D87D4A]">best</span> audio gear
+          </h2>
+          <p class="text-black font-medium opacity-50 mt-10 leading-7">
+            Located at the heart of New York City, Audiophile is the premier
+            store for high end headphones, earphones, speakers, and audio
+            accessories. We have a large showroom and luxury demonstration rooms
+            available for you to browse and experience a wide range of our
+            products. Stop by our store to meet some of the fantastic people who
+            make Audiophile the best place to buy your portable audio equipment.
+          </p>
+        </div>
+        <div>
+          <img src="../assets//shared/desktop/image-best-gear.jpg" />
+        </div>
+      </div>
     </section>
   </main>
 </template>
@@ -121,6 +178,6 @@ import data from "../data.json";
 }
 
 .bg-1 {
-  background-image: url("../assets/shared/desktop/image-zx9-speaker.jpg");
+  background-image: url("../assets//Bitmap.png");
 }
 </style>
