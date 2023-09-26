b9e9e0c4b700b193c6152646d1245ec562c5b85d
iraklikeche <irakli.kechee@gmail.com>
2023-09-26 17:34:50 +0400

    Create cart modal

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -38,21 +38,60 @@
         </template>
       </div>
 
-      <RouterLink to="/">
-        <svg width="23" height="20" xmlns="http://www.w3.org/2000/svg">
+      <div class="relative">
+        <svg
+          width="23"
+          height="20"
+          xmlns="http://www.w3.org/2000/svg"
+          class="cursor-pointer"
+        >
           <path
             d="M8.625 15.833c1.132 0 2.054.935 2.054 2.084 0 1.148-.922 2.083-2.054 2.083-1.132 0-2.054-.935-2.054-2.083 0-1.15.922-2.084 2.054-2.084zm9.857 0c1.132 0 2.054.935 2.054 2.084 0 1.148-.922 2.083-2.054 2.083-1.132 0-2.053-.935-2.053-2.083 0-1.15.92-2.084 2.053-2.084zm-9.857 1.39a.69.69 0 00-.685.694.69.69 0 00.685.694.69.69 0 00.685-.694.69.69 0 00-.685-.695zm9.857 0a.69.69 0 00-.684.694.69.69 0 00.684.694.69.69 0 00.685-.694.69.69 0 00-.685-.695zM4.717 0c.316 0 .59.215.658.517l.481 2.122h16.47a.68.68 0 01.538.262c.127.166.168.38.11.579l-2.695 9.236a.672.672 0 01-.648.478H7.41a.667.667 0 00-.673.66c0 .364.303.66.674.66h12.219c.372 0 .674.295.674.66 0 .364-.302.66-.674.66H7.412c-1.115 0-2.021-.889-2.021-1.98 0-.812.502-1.511 1.218-1.816L4.176 1.32H.674A.667.667 0 010 .66C0 .296.302 0 .674 0zm16.716 3.958H6.156l1.797 7.917h11.17l2.31-7.917z"
             fill="#FFF"
             fill-rule="nonzero"
           />
         </svg>
-      </RouterLink>
+
+        <div
+          class="flex flex-col absolute right-0 top-[-50%] translate-y-[35%] shadow-2xl p-8"
+        >
+          <div class="flex justify-between mb-8">
+            <h4 class="text-black font-bold tracking-wide">CART (3)</h4>
+            <span
+              class="text-black opacity-50 underline hover:text-[#D87D4A] transition-colors cursor-pointer"
+              >Remove All</span
+            >
+          </div>
+          <div class="flex items-center gap-4 mb-8">
+            <img src="https://picsum.photos/50/50" />
+            <div class="flex flex-col justify-between">
+              <span class="font-bold text-[15px]">name</span>
+              <span class="font-bold text-[14px] opacity-50">$342</span>
+            </div>
+
+            <div>
+              <input type="number" placeholder="125" />
+            </div>
+          </div>
+
+          <div class="flex justify-between">
+            <span class="text-black opacity-50">TOTAL</span>
+            <span class="text-black font-bold">$999</span>
+          </div>
+
+          <button
+            class="bg-[#D87D4A] mt-4 px-8 py-3 transform: uppercase tracking-wider text-white text-xs"
+          >
+            Checkout
+          </button>
+        </div>
+      </div>
     </nav>
   </header>
 </template>
 
 <script setup>
-import { ref, computed } from "vue";
+import { computed } from "vue";
 import data from "../data.json";
 
 const uniqueCategories = computed(() => {
